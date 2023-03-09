380f8a5739bdde8eaf213bd915fd1b42c2725b51
Silvia2shine <silvia.padurean@yahoo.com>
2023-03-09 23:10:35 +0200

    Initial commit

---
+++ src/main/java/org/Homework/FirstComparator.java
@@ -0,0 +1,21 @@
+package org.Homework;
+
+
+import java.util.Comparator;
+
+/**
+ * This is a class that implements the Comparator class.
+ */
+class FirstComparator implements Comparator<Person> {
+    /**
+     * This is a method that override the compare method.
+     *
+     * @param e1 – the first object to be compared;
+     * @param e2 – the second object to be compared;
+     */
+    @Override
+    public int compare(Person e1, Person e2) {
+        return (e1.name).compareTo(e2.name);
+    }
+}
+
---
+++ src/main/java/org/Homework/HobbysForMap.java
@@ -0,0 +1,40 @@
+package org.Homework;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * This is a class that contains 3 methods.
+ *
+ * @param : - nameOfHobbiesForMap - the name of the hobby's, used to create the MAp;
+ */
+public class HobbysForMap {
+    String nameOfHobbiesForMap;
+
+    /**
+     * This is the constructor of the class HobbysForMap.
+     *
+     * @param : - name - the name of the hobby's;
+     */
+    HobbysForMap(String name) {
+        this.nameOfHobbiesForMap = name;
+
+    }
+    /**
+     * This is a public method that get the name of the hobby's. ;
+     * @return nameOfHobbiesForMap;
+     */
+    public String getName() {
+        return nameOfHobbiesForMap;
+
+    }
+    /**
+     * This is a  method that override the String.
+     *  @return nameOfHobbiesForMap;
+     */
+    @Override
+    public String toString() {
+        return "Hobbys {" +
+                "name='" + nameOfHobbiesForMap + '\'' +
+                '}';
+    }
+}
